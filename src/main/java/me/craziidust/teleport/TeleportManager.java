fad53a3f5a302db9b93aad79528e18c70552f979
CraziiDust <pasztorlevi2000@gmail.com>
2023-05-19 15:16:15 +0200

    05.19 - Paper függőség hozzáadása - TeleportManager interfész

--- src/main/java/me/craziidust/teleport/TeleportManager.java
+++ src/main/java/me/craziidust/teleport/TeleportManager.java
@@ -1,2 +1,35 @@
-package me.craziidust.teleport;public interface TeleportManager {
+package me.craziidust.teleport;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public interface TeleportManager {
+
+    default void senderToTarget(Player sender, Player target) {
+        senderToTarget(sender,target,false);
+    }
+
+    void senderToTarget(Player sender,Player target, boolean instant);
+
+    default void targetToSender(Player sender, Player target) {
+        targetToSender(sender,target,false);
+    }
+
+    void targetToSender(Player sender, Player target, boolean instant);
+
+    default void senderToLocation(Player sender, Location location){
+        senderToLocation(sender,location,false);
+    }
+
+    void senderToLocation(Player player, Location location, boolean instant);
+
+    void cancelTeleport(Player player);
+
+    void acceptTeleport(Player target, Player sender);
+
+    void refuseTeleport(Player target, Player sender);
+
+    List<Player> getTeleportRequests(Player target);
 }
