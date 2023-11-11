f28a7367e56f2418ff16bd060ffc35c97de45323
CraziiDust <pasztorlevi2000@gmail.com>
2023-11-11 19:02:05 +0100

    New MenuHolder structure

--- src/main/java/me/craziidust/craziilib/menu/MenuHolder.java
+++ src/main/java/me/craziidust/craziilib/menu/MenuHolder.java
@@ -1,6 +1,29 @@
 package me.craziidust.craziilib.menu;
 
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
 public interface MenuHolder {
 
-    Menu getMenu();
+    interface SingeMenuHolder {
+
+        Menu getMenu();
+
+        default void open(Player player) {
+            getMenu().open(player);
+        }
+    }
+
+    interface MultipleMenuHolder {
+
+        Map<String, Menu> getMenus();
+
+        default Menu getMenu(String id) {
+            return getMenus().get(id);
+        }
+        default void open(Player player, String id) {
+            getMenu(id).open(player);
+        }
+    }
 }
