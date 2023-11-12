ee314dc7b93b94eeeb3c4299f55f729818a18af0
DustKing5 <pasztorlevi2000@gmail.com>
2023-11-12 11:24:44 +0100

    Simplify MenuHolder

--- src/main/java/me/craziidust/craziilib/menu/MenuHolder.java
+++ src/main/java/me/craziidust/craziilib/menu/MenuHolder.java
@@ -1,38 +1,20 @@
 package me.craziidust.craziilib.menu;
 
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.TestOnly;
 
 import java.util.Map;
 
 public interface MenuHolder {
 
-    interface SingleMenuHolder {
+    Menu getMenu(@Nullable Player player, @Nullable String id);
 
-        Menu getMenu();
-
-        default void open(Player player) {
-            getMenu().open(player);
-        }
+    default Menu getMenu(Player player) {
+        return getMenu(player, null);
     }
 
-    interface MultipleMenuHolder {
-
-        Map<String, Menu> getMenus();
-
-        default Menu getMenu(String id) {
-            return getMenus().get(id);
-        }
-        default void open(Player player, String id) {
-            getMenu(id).open(player);
-        }
-    }
-
-    interface PerPlayerMenuHolder {
-
-        Menu getMenu(Player player);
-
-        default void open(Player player) {
-            getMenu(player).open(player);
-        }
+    default Menu getMenu(String id) {
+        return getMenu(null, id);
     }
 }
