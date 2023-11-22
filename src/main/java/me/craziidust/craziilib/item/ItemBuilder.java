d04b3bf59d1de8c75dc7842f1a653a7bb0f76a8c
DustKing5 <pasztorlevi2000@gmail.com>
2023-11-22 17:41:24 +0100

    ItemBuilder Support PersistentData, existing ItemStack ant all Component

--- src/main/java/me/craziidust/craziilib/item/ItemBuilder.java
+++ src/main/java/me/craziidust/craziilib/item/ItemBuilder.java
@@ -1,14 +1,16 @@
 package me.craziidust.craziilib.item;
 
 import me.craziidust.craziilib.item.builders.MetaBuilder;
-import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.Range;
 
 import java.util.List;
@@ -19,6 +21,10 @@ public class ItemBuilder {
 
     public ItemBuilder(Material material) {
         this.itemStack = new ItemStack(material);
+    }
+
+    public ItemBuilder(ItemStack itemStack) {
+        this.itemStack = itemStack;
     }
 
     public ItemBuilder(Material material, @Range(to = 0, from = 64) int amount) {
@@ -50,19 +56,72 @@ public class ItemBuilder {
         return this;
     }
 
-    public ItemBuilder display(TextComponent textComponent) {
+    public ItemBuilder display(Component textComponent) {
         itemStack.editMeta(itemMeta -> itemMeta.displayName(textComponent));
         return this;
     }
 
-    public ItemBuilder lore(TextComponent ...lines) {
+    public ItemBuilder lore(Component ...lines) {
         itemStack.editMeta(itemMeta -> itemMeta.lore(List.of(lines)));
         return this;
     }
 
-    public ItemBuilder lore(List<TextComponent> lore) {
+    public ItemBuilder lore(List<Component> lore) {
         itemStack.editMeta(itemMeta -> itemMeta.lore(lore));
         return this;
+    }
+
+    public <Z> ItemBuilder tag(NamespacedKey key, PersistentDataType<?,Z> type, Z value) {
+        itemStack.editMeta(itemMeta -> itemMeta.getPersistentDataContainer().set(key,type,value));
+        return this;
+    }
+
+    public <Z> ItemBuilder tag(String key, PersistentDataType<?, Z> type, Z value) {
+        return tag(NamespacedKey.fromString(key), type, value);
+    }
+
+    public ItemBuilder tag(String key, boolean value) {
+        return tag(key, PersistentDataType.BOOLEAN, value);
+    }
+
+    public ItemBuilder tag(String key, byte value) {
+        return tag(key, PersistentDataType.BYTE, value);
+    }
+
+    public ItemBuilder tag(String key, byte ...value) {
+        return tag(key, PersistentDataType.BYTE_ARRAY, value);
+    }
+
+    public ItemBuilder tag(String key, double value) {
+        return tag(key, PersistentDataType.DOUBLE, value);
+    }
+
+    public ItemBuilder tag(String key, float value) {
+        return tag(key, PersistentDataType.FLOAT, value);
+    }
+
+    public ItemBuilder tag(String key, int value) {
+        return tag(key, PersistentDataType.INTEGER, value);
+    }
+
+    public ItemBuilder tag(String key, int ...value) {
+        return tag(key, PersistentDataType.INTEGER_ARRAY, value);
+    }
+
+    public ItemBuilder tag(String key, long value) {
+        return tag(key, PersistentDataType.LONG, value);
+    }
+
+    public ItemBuilder tag(String key, long ...value) {
+        return tag(key, PersistentDataType.LONG_ARRAY, value);
+    }
+
+    public ItemBuilder tag(String key, short value) {
+        return tag(key, PersistentDataType.SHORT, value);
+    }
+
+    public ItemBuilder tag(String key, String value) {
+        return tag(key, PersistentDataType.STRING, value);
     }
 
     public ItemBuilder cloneMeta(ItemStack src) {
