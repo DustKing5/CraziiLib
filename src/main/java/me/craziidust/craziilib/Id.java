6db27ddf9c8e658771f6d4db0f32d4121047591e
DustKing5 <pasztorlevi2000@gmail.com>
2023-12-30 13:03:57 +0100

    Id is only string

--- src/main/java/me/craziidust/craziilib/Id.java
+++ src/main/java/me/craziidust/craziilib/Id.java
@@ -5,31 +5,11 @@ import java.util.Random;
 public class Id {
 
     private static final Random RANDOM = new Random();
-    private final String id;
 
-    public Id(String id) {
-        this.id = id;
-    }
-
-    public Id() {
-        id = RANDOM.ints(97,123)
+    public String get() {
+        return RANDOM.ints(97,123)
                 .limit(5)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append
                 ).toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Id id1 = (Id) o;
-
-        return id.equals(id1.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
     }
 }
